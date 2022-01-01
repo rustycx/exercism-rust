a5e0fec93f76903829b27ab177c1d4250ea78b1e
rustycx <rustycx@gmail.com>
2021-12-31 22:56:29 -0800

    beer-song

---
+++ beer-song/src/lib.rs
@@ -0,0 +1,55 @@
+fn get_bottles(n: u32) -> String {
+    match n {
+        1 => "1 bottle".into(),
+        _ => format!("{} bottles", n),
+    }
+}
+
+pub fn verse0(n: u32) -> String {
+    if n == 0 {
+        "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n".into()
+    } else if n == 1 {
+        "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n".into()
+    } else {
+        format!(
+            "{} of beer on the wall, {} of beer.\nTake one down and pass it around, {} of beer on the wall.\n",
+            get_bottles(n),
+            get_bottles(n),
+            get_bottles(n - 1)
+        )
+    }
+}
+
+pub fn sing0(start: u32, end: u32) -> String {
+    (end..=start).rev().fold(String::new(), |mut s, n| {
+        s.push_str(&verse0(n));
+        if n != end {
+            s.push('\n');
+        }
+        s
+    })
+}
+
+// https://exercism.org/tracks/rust/exercises/beer-song/solutions/bernardoamc
+const VERSES: [&'static str; 3] = [
+    "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n",
+    "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n",
+    "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
+];
+pub fn verse(n: u16) -> String {
+    match n {
+        0 => VERSES[0].to_string(),
+        1 => VERSES[1].to_string(),
+        2 => VERSES[2].to_string(),
+        _ => format!(
+            "{0} bottles of beer on the wall, {0} bottles of beer.\n\
+                   Take one down and pass it around, {1} bottles of beer \
+                   on the wall.\n",
+            n,
+            n - 1
+        ),
+    }
+}
+pub fn sing(from: u16, to: u16) -> String {
+    (to..=from).rev().map(|x| verse(x)).collect::<Vec<_>>().join("\n")
+}
